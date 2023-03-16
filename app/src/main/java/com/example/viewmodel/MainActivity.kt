a81e1b6dac4afe80af19e2346747cf482652b22c
Md.Tareq Islam <103177152+Tareq1131@users.noreply.github.com>
2023-03-16 14:50:14 +0600

    add fun setText

--- app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textCounter:TextView
+    var count:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textCounter = findViewById(R.id.textTV)
+        setText()
+    }
+
+     fun setText() {
+       
     }
 }
