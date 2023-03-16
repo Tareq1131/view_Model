f5e3b9443f5ac7ea9d09ade5737f498016cd2746
Md.Tareq Islam <103177152+Tareq1131@users.noreply.github.com>
2023-03-16 17:51:09 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -2,22 +2,28 @@ package com.example.viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var textCounter:TextView
+    lateinit var mainViewModel: MainViewModel //object for viewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         textCounter = findViewById(R.id.textTV)
         setText()
     }
 
 
      fun setText() {
-         textCounter.text = count.toString()
+         textCounter.text = mainViewModel.toString()
     }
+}
+
+private fun ViewModelProvider.get(modelClass: Class<MainViewModel>): MainViewModel {
+
 }
